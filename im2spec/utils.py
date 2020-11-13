ea337c581000f8c806ac8a72522bd827f27fac17
ziatdinovmax <ziatdinovmax@gmail.com>
2020-11-13 08:58:26 +0000

    Add util funcs for latent space analysis

--- im2spec/utils.py
+++ im2spec/utils.py
@@ -2,8 +2,8 @@ from typing import Tuple, Type, Union
 
 import numpy as np
 import torch
-
 from shapely.geometry import Polygon
+from sklearn.mixture import GaussianMixture
 
 
 def predict(model, feature_arr: Union[np.ndarray, torch.Tensor]) -> np.ndarray:
@@ -14,6 +14,8 @@ def predict(model, feature_arr: Union[np.ndarray, torch.Tensor]) -> np.ndarray:
     if isinstance(feature_arr, np.ndarray):
         feature_arr = torch.from_numpy(feature_arr).float()
     feature_arr = feature_arr.to(device)
+    model.eval()
+    model.to(device)
     with torch.no_grad():
         prediction = model(feature_arr)
     prediction = prediction.cpu().numpy()
@@ -51,7 +53,7 @@ def make_window(imgsrc: np.ndarray, window_size: int,
     Returns the portion of the image within the window given the
     image (imgsrc), the x position and the y position
     """
-    imgsrc = imgsrc[int(xpos-window_size/2):int(xpos+window_size/2), 
+    imgsrc = imgsrc[int(xpos-window_size/2):int(xpos+window_size/2),
                     int(ypos-window_size/2):int(ypos+window_size/2)]
     return imgsrc
 
@@ -96,3 +98,65 @@ def loop_area(pred_loop: np.ndarray,
     target_area = np.array([Polygon(p).area for p in polygons])
     area_error = np.abs(pred_area-target_area)
     return pred_area, target_area, area_error
+
+
+def encode(model: Type[torch.nn.Module],
+           feature_arr: Union[torch.Tensor, np.ndarray],
+           ) -> Tuple[np.ndarray]:
+    """
+    Encodes features in a latent space
+    """
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    if isinstance(feature_arr, np.ndarray):
+        feature_arr = torch.from_numpy(feature_arr).float()
+    feature_arr = feature_arr.to(device)
+    model.eval()
+    model.to(device)
+    with torch.no_grad():
+        latent_features = model.encoder(feature_arr)
+    return latent_features.cpu().numpy()
+
+
+def encode_gmm(model: Type[torch.nn.Module],
+               feature_arr: Union[torch.Tensor, np.ndarray],
+               nc: int) -> Tuple[np.ndarray]:
+    """
+    Encodes features in latent space and peforms Gaussian mixture model-based
+    classification of the latent space
+    """
+    latent_features = encode(model, feature_arr)
+    gmmp = GaussianMixture(n_components=nc, random_state=42)
+    gmm_zp = gmmp.fit_predict(latent_features)
+    centroids = gmmp.fit(latent_features).means_
+    return latent_features, gmm_zp, centroids
+
+
+def decode(model: Type[torch.nn.Module],
+           latent_coord: Union[np.ndarray, torch.Tensor],
+           ) -> np.ndarray:
+    """
+    Projects latent coordinates to a signal space
+    """
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    if isinstance(latent_coord, np.ndarray):
+        latent_coord = torch.from_numpy(latent_coord).float()
+    latent_coord = latent_coord.to(device)
+    model.eval()
+    model.to(device)
+    with torch.no_grad():
+        decoded = model.decoder(latent_coord)
+    return decoded.cpu().numpy()
+
+
+def latent_gmm(model: Type[torch.nn.Module],
+               feature_arr: Union[torch.Tensor, np.ndarray],
+               nc: int) -> Tuple[np.ndarray]:
+    """
+    Encodes a feature array in latent space, performs Gaussian mixture model
+    based classification and projects centroids of each mixture component
+    to a signal space
+    """
+    latent_features, gmm_zp, centroids = encode_gmm(model, feature_arr, nc)
+    decoded_centroids = decode(model, centroids)
+    return latent_features, gmm_zp, centroids, decoded_centroids
+
